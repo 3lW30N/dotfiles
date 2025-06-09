5441f464417d1b0dc416a997140f2353aeb216f5
3lW30N <noemierialland2004@gmail.com>
2025-06-09 17:52:54 +0200

    chore: update config

--- sketchybar/plugins/calendar.sh
+++ sketchybar/plugins/calendar.sh
@@ -3,8 +3,4 @@
 # Heure française
 TIME_FR=$(TZ="Europe/Paris" date "+%H:%M")
 
-# Heure de Montréal  
-TIME_MTL=$(TZ="America/Montreal" date "+%H:%M")
-
-# Gardez l'icône avec la date comme avant
-sketchybar --set $NAME icon="$(date '+%a %d. %b')" label="$TIME_FR  $TIME_MTL"
+sketchybar --set $NAME icon="$(date '+%a %d. %b')" label="$TIME_FR"
